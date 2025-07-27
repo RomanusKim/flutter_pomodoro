096e45b774061920c4e32f9f13daaaa5a3bc5b41
romanus <romanusdev@gmail.com>
2025-07-27 21:28:12 +0900

    #dev define UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
+        textTheme: TextTheme(
+          headlineSmall: TextStyle(color: Color(0xFF232B55)),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
+      home: Scaffold(body: HomeScreen()),
     );
   }
 }
